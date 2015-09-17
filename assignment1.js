0ced6c8b47c38dbc488dc9891d8d19d439f8ed67
khanhtrn <khanhtrn@gmail.com>
2015-09-17 13:35:26 -0700

    Assignment1 Finished v1

---
+++ assignment1.js
@@ -0,0 +1,126 @@
+//STEP 1
+//var someMonth;
+//function(theMonth)
+//const currentMonth;
+//var summerMonth;
+//function(myLibraryA)
+//STEP 2
+//49.41029              //numeric literal
+//"What the hell!!!"    //string literal
+//true                  //boolean literal
+//null                  //null literal
+//STEP 3
+//var complexExpression = 53 + (64 / 23) - (15 * 92);
+//var variableExpression = ("What" + "" + "the" + "" + "hell?")
+//STEP 4
+//var firstName, lastName, myAddress, myCity, myState, myZip, myAge, myReferralSource, myMayContact;
+//STEP 5
+//Method 1
+//var firstName;
+//var lastName;
+//var myAddress;
+//var myCity;
+//var myState;
+//var myZip;
+//var myAge;
+//var myReferralSource;
+//var myMayContact;
+//firstName = "khanh";
+//lastName = "tran";
+//myAddress = "123 Some st";
+//myCity = "Anytown";
+//myState = "CA";
+//myZip = 12345;
+//myAge = 75;
+//myReferralSource = "some random site";
+//myMayContact = "maybe";
+//Method 2
+//var firstName = "khanh";
+//var lastName = "tran";
+//var myAddress = "123 Some st";
+//var myCity = "Anytown";
+//var myState = "CA";
+//var myZip = 12345;
+//var myAge = 75;
+//var myReferralSource = "some random site;
+//var myMayContact = "maybe";
+//Method 3
+//var firstName = "khanh", lastName = "tran", myAddress = "123 Some st", myCity = "Anytown", myState = "CA", myZip = 12345, myAge = 75, myReferralSource = "some random site", myMayContact = "maybe";
+//STEP 6
+//var yearsStudent = 99;
+//console.log("I have been a student for " + yearsStudent + " years.");
+//var isStudent = true;
+//console.log("Am I a student? " + isStudent);
+//var isBoolean = false;
+//console.log(yearsStudent + isBoolean);
+//STEP 7
+//var myName;
+//console.log(myName);
+//myName = "khanh tran";
+//STEP 8
+//var someString = 'Albert Einstein said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+//console.log(someString); //added backslash between I \' m
+//STEP 9
+//var nulVal = null;
+//var myName;
+//console.log(nulVal);
+//console.log(myName);
+//STEP 10
+//var str = "This might be a string!";
+//var num = 93586868398;
+//var boo = false; 
+//var obj = new Object();
+//var undef;
+//console.log(typeof(str));
+//console.log(typeof(num));
+//console.log(typeof(boo));
+//console.log(typeof(obj));
+//console.log(typeof(undef));
+//STEP 11
+//window.alert("Hello " + "khanh tran" + ", welcome to the JavaScript class!");
+//STEP 12
+//var name = "khanh tran";
+//window.alert("Hello " + name + ", welcome to the JavaScript class!");
+//STEP 13
+//var course = "JavaScript";
+//window.alert("Hello " + "khanh tran" + ", welcome to the " + course +" class!");
+//STEP 14
+//var course = "JavaScript";
+//window.alert("Hello " + "khanh tran." + "\nWelcome to the " + course +" class!");
+//STEP 15
+//var yourName = window.prompt("Type something here that resemble a name: ");
+//window.alert("Hello " + yourName + ", welcome to the JavaScript class!");
+//STEP 16
+//var yourCourse = window.prompt("Type the name of the class you're taking: ");
+//window.alert("Hello " + "khanh tran." + "\nWelcome to the " + yourCourse +" class!");
+//STEP 17
+//var x = 10;
+//var y = 20;
+//console.log(x + y);
+//STEP 18
+//var x = 20;
+//console.log(x+=20);
+//STEP 19
+//var x = 20;
+//console.log(x*=5);
+//STEP 20
+//var x = (20 % 3);
+//console.log(x/=1);
+//STEP 21
+//var x = 25;
+//var y = 50;
+//console.log(x < y && y > x);
+//STEP 22
+//var x = 50;
+//var y = 50;
+//console.log(x <= y != true);
+//STEP 23
+//var widget = new Object();
+//console.log(typeof(widget));
+//STEP 24
+//var widget = new Object();
+//console.log(widget instanceof(Object));
+//STEP 25
+//var widget = new Object();
+//console.log(widget instanceof(String));
+
